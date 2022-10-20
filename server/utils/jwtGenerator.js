3d3b28a65cf492466682132d0ee1e92cb3787292
Jonape P. Cabug <111430725+CheeksXV@users.noreply.github.com>
2022-10-20 11:00:02 +0800

    Built private route

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 
 function jwtGenerator(user_id) {
   const payload = {
-    user: user_id,
+    user: {
+      id: user_id,
+    },
   };
 
-  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1hr" });
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
 }
 
 module.exports = jwtGenerator;
