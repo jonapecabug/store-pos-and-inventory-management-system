e8d1d6a7339f2b99e618bf9590b9a7adf8c59e3c
Jonape P. Cabug <111430725+CheeksXV@users.noreply.github.com>
2022-10-20 11:00:02 +0800

    Built private route

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -4,19 +4,19 @@ require("dotenv").config();
 module.exports = async (req, res, next) => {
   try {
     //1. desctructure
-
     const jwtToken = req.header("token");
 
     if (!jwtToken) {
-      return res.status(403).json("Unauthorize access.");
+      return res.status(403).json("no token");
     }
 
     //check if token is valid
-    const payload = jwt.verify(jwtToken, process.env.jwtsecret);
+    const payload = jwt.verify(jwtToken, process.env.jwtSecret);
 
     req.user = payload.user;
+    next();
   } catch (err) {
     console.error(err.message);
-    return res.status(403).json("Unauthorize access.");
+    return res.status(403).json("You are not authorized.");
   }
 };
